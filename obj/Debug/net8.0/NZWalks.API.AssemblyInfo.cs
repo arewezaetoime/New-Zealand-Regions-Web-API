80de4cc90745d46cb463bad38a20038bc71046aa
arewezaetoime <hristoiliev.1994@gmail.com>
2024-11-09 19:15:04 +0200

    Creating ASP.NET MVC Web Application

--- obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56da7772c12c23f1af3f7629f3b0a6a8d5100f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5216232380aa2c4169eb549b601f51a128bfaf3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
