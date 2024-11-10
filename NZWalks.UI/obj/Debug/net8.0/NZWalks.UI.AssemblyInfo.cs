1ea7c360f41d0172a1d64dd093c56f35f1490841
arewezaetoime <hristoiliev.1994@gmail.com>
2024-11-10 11:33:54 +0200

    Edit some UI controllers, views as well gitignore

--- NZWalks.UI/obj/Debug/net8.0/NZWalks.UI.AssemblyInfo.cs
+++ NZWalks.UI/obj/Debug/net8.0/NZWalks.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c4ca148f81de9401f8b8bf5d53b55974ea405f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
